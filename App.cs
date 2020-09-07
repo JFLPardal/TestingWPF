7de50844db598e867937c6b369f6676bb35aeade
JoaoPardal <jflpardal@gmail.com>
2020-09-07 16:09:23 +0100

    4 Skills are being retrieved and displayed as a list

---
+++ App.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TestingWPF
+{
+    public partial class App : Application
+    {
+        public ObservableCollection<Skill> SkillsList { get; set; } = new ObservableCollection<Skill>();
+
+        private void InitApp(object sender, StartupEventArgs args)
+        {
+            LoadSkills();
+        }
+
+        private void LoadSkills()
+        {
+            // Occultist's skills
+            var SacrificialStab = new Skill("Sacrificial Stab", 4, 80, 10);
+            var AbyssalArtillery = new Skill("Abyssal Artillery", 5, 80, 5);
+            var WeakningCurse = new Skill("Weakning Curse", 2, 90, 5);
+            var DaemonsPull = new Skill("Daemon's Pull", 3, 80, 17);
+
+            // add them to SkillsList
+            SkillsList.Add(SacrificialStab);
+            SkillsList.Add(AbyssalArtillery);
+            SkillsList.Add(WeakningCurse);
+            SkillsList.Add(DaemonsPull);
+        }
+    }
+}
---
+++ Skill.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingWPF
+{
+    public class Skill
+    {
+        private string m_name;
+        private int m_damage;
+        private int m_accuracy;
+        private int m_critChance;
+
+        public Skill(string name, int damage, int accuracy, int critChance)
+        {
+            m_name = name;
+            m_damage = damage;
+            m_accuracy = accuracy;
+            m_critChance = critChance;
+        }
+
+        public string Name
+        {
+            get { return m_name; }
+        }
+    }
+}
